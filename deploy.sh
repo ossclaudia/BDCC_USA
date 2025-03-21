9cc89b1daa8ccab7d9c622b36012d224402c15fc
ossclaudia <claudiaclaudiia03@gmail.com>
2025-03-21 18:05:17 +0000

    .

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-gcloud functions deploy update_statistics \
-    --runtime python311 \
-    --trigger-http \
-    --allow-unauthenticated \
-    --region=europe-southwest1 \
-    --entry-point=update_statistics \
-    --source=.
-
-
--- deploy_schedule.sh
+++
@@ -1,8 +0,0 @@
-#gcloud config set project barbara2-451412
-
-gcloud scheduler jobs create http update-statistics-job \
-    --schedule "*/10 * * * *" \
-    --uri "https://europe-southwest1-barbara2-451412.cloudfunctions.net/update_statistics" \
-    --http-method=GET \
-    --time-zone="Europe/Lisbon" \
-    --description="Updates admissions statistics every 10 minutes"
